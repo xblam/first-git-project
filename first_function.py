ffeb0c73e1f05627a2a38a5fc1dcc650050481ea
ben lam <benlam05032004@gmail.com>
2022-07-22 11:28:53 +0700

    bolden charater if character in word

--- first_function.py
+++ first_function.py
@@ -2,9 +2,11 @@ def check_guess(guess, word):
     answer = ''
     for i in range(len(word)):
         if guess[i] == word[i]:
-            answer += word[i]
+            answer += guess[i]
+        elif guess[i] in word:
+            answer += guess[i].upper()
         else:
             answer += '*'
     return answer
 
-print(check_guess('benny','denns'))
+print(check_guess('benny','denna'))
